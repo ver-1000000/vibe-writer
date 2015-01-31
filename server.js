5a7756225d81581975cee1afa49b3b6d9ce5f317
akai@archHome <vermilion928@gmail.com>
2015-02-01 07:55:21 +0900

    すごーい(´・ω・｀)

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ var app = require('http').createServer(function(req, res) {
     } else if (req.url == '/jquery.min.js'){
       res.writeHead(200, {'Content-Type': 'text/javascript'});
       fs.createReadStream('jquery.min.js').pipe(res);
+    } else if (req.url == '/jquery.jrumble.1.3.min.js'){
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      fs.createReadStream('jquery.jrumble.1.3.min.js').pipe(res);
     } else if (req.url == '/common.css'){
       res.writeHead(200, {'Content-Type': 'text/css'});
       fs.createReadStream('common.css').pipe(res);
+    } else if (req.url == '/dict.csv'){
+      res.writeHead(200, {'Content-Type': 'text/csv'});
+      fs.createReadStream('dict.csv').pipe(res);
     } else {
       res.writeHead(200, {'Content-Type': 'text/html'});
       fs.createReadStream('view.html').pipe(res);
@@ -21,5 +27,11 @@ io.sockets.on('connection', function(socket) {
   });
   socket.on('vibes', function(data) {
     io.sockets.emit('vibes', data);
+  });
+  socket.on('negotiation', function() {
+    io.sockets.emit('negotiation');
+  });
+  socket.on('now', function(data) {
+    io.sockets.emit('now', data);
   });
 });
