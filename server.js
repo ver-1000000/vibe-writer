c3f77dac1c821dcda2b6810fdcdcfa7aeabc235f
akai@archHome <vermilion928@gmail.com>
2015-01-31 20:26:58 +0900

    more fix

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = require('http').createServer(function(req, res) {
     if (req.url == '/'){
       res.writeHead(200, {'Content-Type': 'text/html'});
       fs.createReadStream('index.html').pipe(res);
+    } else if (req.url == '/jquery.min.js'){
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      fs.createReadStream('jquery.min.js').pipe(res);
     } else if (req.url == '/common.css'){
       res.writeHead(200, {'Content-Type': 'text/css'});
       fs.createReadStream('common.css').pipe(res);
@@ -13,7 +16,10 @@ var app = require('http').createServer(function(req, res) {
 }).listen(3000);
 var io = require('socket.io').listen(app);
 io.sockets.on('connection', function(socket) {
-  socket.on('msg', function(data) {
-    io.sockets.emit('msg', data);
+  socket.on('chara', function(data) {
+    io.sockets.emit('chara', data);
+  });
+  socket.on('vibes', function(data) {
+    io.sockets.emit('vibes', data);
   });
 });
